15a25746730252032855d8bf4e3da95b856769a7
Eshu-M <eshambehredin20@gmail.com>
2023-09-13 17:30:43 -0700

    added the header for the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
+import HomeHeader from '@/components/HomeHeader'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <>
+    <HomeHeader/>
+    </>
   )
 }
