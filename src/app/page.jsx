7150f5e65a0f8f6e4bc40084fd79e4115268cc39
Eshu-M <eshambehredin20@gmail.com>
2023-09-14 17:26:13 -0700

    added the body of the home page and functionality

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,20 @@
 import HomeHeader from '@/components/HomeHeader'
+import HomeSearch from '@/components/HomeSearch'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
+    {/*Header */}
+
     <HomeHeader/>
+
+    {/*Body */}
+
+    <div className='flex flex-col items-center mt-24'>
+      <Image width={'300'} height={'100'} src={'/Google-Logo.png'}/>
+      <HomeSearch/>
+    </div>
     </>
   )
 }
