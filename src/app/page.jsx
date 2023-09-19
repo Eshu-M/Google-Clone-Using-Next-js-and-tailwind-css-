ec9b4cf3f11ca7121d3c18973660531b42dcac52
Eshu-M <eshambehredin20@gmail.com>
2023-09-18 18:23:02 -0700

    fixing home page v2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Image width={'300'} height={'100'} src={'/Google-Logo.png'} alt='Google Logo'/>
       <HomeSearch/>
     </div>
-    <div className='flex flex-col justify-center items-center mt-16 pb-30'>
+    <div className='flex flex-col justify-center items-center mt-5 pb-60'>
       <Image width={'150'} height={'50'} src={'/Google-Logo.png'} alt='Google Logo'/>
       <HomeSearch/>
     </div>
